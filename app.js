2ad721d5b537777a5d552e4ba031b83830ec3fb0
wesley <cheung.w.2016@gmail.com>
2024-01-25 15:29:28 -0800

    added models, routes, validations for login and register of users

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
-const express = require('express');
+// libraries
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+// database URI
+const db = require("./config/keys").mongoURI;
+
+// routes/api
+const users = require("./routes/api/users");
+const games = require("./routes/api/games");
+
+// models
+const User = require("./models/User");
+
+mongoose
+    .connect(db, {useNewUrlParser: true})
+    .then(() => console.log("connected to mongoDB"))
+    .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
+    const user = new User({
+        handle: "test",
+        email: "testing@gmail.com",
+        password: "password"
+    })
+    user.save()
     res.send("Hello Everyone!!");
 });
+
+app.use("/api/users", users)
+app.use("/api/games", games)
+
 
 const port = process.env.PORT || 5000;
 
---
+++ routes/api/users.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+const keys = require("../../config/keys");
+const jwt = require("jsonwebtoken");
+
+// validate
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
+
+router.get("/test", (req,res) => {
+    res.json({msg: "This is the user route"});
+});
+
+// sign up
+router.post('/register', (req,res) => {
+    const { errors, isValid } = validateRegisterInput(req.body)
+
+    if(!isValid){
+        return res.status(400).json(errors)
+    }
+
+    User.findOne({ email: req.body.email })
+    .then(user => {
+        if (user) {
+            return res.status(400).json({email: "A user is already registered with that email."})
+        } else {
+            const newUser = new User({
+                handle: req.body.handle,
+                email: req.body.email,
+                password: req.body.password
+            })
+            
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                    .then(user => res.json(user))
+                    .catch(err => console.log(err));
+                })
+            })
+        }
+    })
+})
+
+// login
+router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body)
+
+  if(!isValid){
+    return res.status(400).json(errors)
+  }
+  
+  User.findOne({email: req.body.email}).then(
+    user => {
+      if(!user){
+        return res.status(404).json({email: "This user does not exist"})
+      }
+
+      bcrypt.compare(req.body.password, user.password).then(isMatch => {
+        if (!!isMatch){
+          const payload = {
+            id: user.id, //mongo db object id
+            handle: user.handle,
+            email: user.email,
+            currency: user.currency,
+          }
+
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            { 
+              expiresIn: 3600
+            },
+            (err, token) => {
+              res.json({
+                sucess: true, 
+                token: "Bearer" + token 
+              })
+            }
+          )
+        } else {
+          return res.status(400).json({password: "Incorrect password"})
+        }
+      })
+    }
+  )
+})
+
+module.exports = router;
