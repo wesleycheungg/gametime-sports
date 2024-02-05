ec2ced5f29ced54fb0f9312296446659e608391f
wesley <cheung.w.2016@gmail.com>
2024-02-05 10:53:14 -0800

    add tomorrows date function to getGamesNBAapi

--- utils/getGamesNBAapi.js
+++ utils/getGamesNBAapi.js
@@ -2,14 +2,28 @@ var axios = require('axios');
 const rapidAPI_key = require('../config/keys').rapidAPI_key;
 const rapidAPI_host = require('../config/keys').rapidApi_host;
 
+// need to get tomorrows date because api gets todays actual games with tomorrows date as param
+const getTomorrowsDate = () => {
+  const today = new Date();
+  // console.log(today)
+  today.setDate(today.getDate() + 1); // get tomorrows date
+
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, '0');
+  const day = String(today.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
+
 const getGameResultsFromNBAapi = () => {
+  const dateForTodaysGames = getTomorrowsDate()
+
   const options = {
     method: 'GET',
     url: 'https://api-nba-v1.p.rapidapi.com/games',
-    params: {date: '2024-02-02'},
+    params: {date: dateForTodaysGames},
     headers: {
       'X-RapidAPI-Key': rapidAPI_key,
-      'X-RapidAPI-Host': rapidApi_host
+      'X-RapidAPI-Host': rapidAPI_host
     }
   };
 
